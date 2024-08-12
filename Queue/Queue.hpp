dab74e49d732547693a4dee2053573a621826572
Plamen Stoynev <79408379+plamenstoynev@users.noreply.github.com>
2024-08-12 10:12:18 +0300

    Add Queue.hpp

--- Queue/Queue.hpp
+++ Queue/Queue.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdexcept>
 
 
 template <class T>
@@ -37,3 +38,42 @@ Queue<T>::Queue(size_t size) {
     this->data = new T[size + 1];
 }
 
+template<class T>
+void Queue<T>::enqueue(const T &data) {
+    if(!isFUll()) {
+        this->data[this->end] = data;
+        this.end = (this->end + 1) % this.size;
+    }
+    else
+        throw std::overflow_error("Queue is full");
+}
+
+template<class T>
+T Queue<T>::dequeue() {
+        const T& result = first();
+        this->begin = (this->begin + 1) % this->size;
+        return result;
+}
+
+template<class T>
+const T &Queue<T>::first() const {
+    if(!isEmpty())
+        return this->data[this->begin];
+    else
+        throw std::underflow_error("Queue is empty");
+}
+
+template<class T>
+bool Queue<T>::isEmpty() const {
+    return this->begin == this->end;
+}
+
+template<class T>
+bool Queue<T>::isFUll() const {
+    return (this->end + 1) % this->size == this->begin;
+}
+
+template<class T>
+size_t Queue<T>::count() const {
+    return (this->end + this->size - this->begin) % this->size;
+}
